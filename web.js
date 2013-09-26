e784e53f2d82f5310372c224103a68e9fd02c5ee
Ubuntu <ubuntu@ip-10-185-27-10.ec2.internal>
2013-09-26 03:51:44 +0000

    added index.html reference

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ var fs = require("fs");
 var buf = fs.readFileSync("html/index.html");
 var index = buf.toString();
 
+app.get('/', function(request, response) {
+    response.send(index);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);

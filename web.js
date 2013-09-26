e69e75f25a19b4e887dd3b35d8ae933624a7c918
Ubuntu <ubuntu@ip-10-185-27-10.ec2.internal>
2013-09-26 03:41:09 +0000

    added index.html reference

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send(fs.readFileSync('/index.html', 'utf8'));
+app.get('/', function (request, response) {
+    response.sendfile('index.html');
 });
 
 var port = process.env.PORT || 5000;
